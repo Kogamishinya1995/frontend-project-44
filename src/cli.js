5b5c113ac9bb6785bd9803c15700972542fe8a65
aleksandr <79081103268@yandex.ru>
2024-01-09 21:59:58 +0600

    working code

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export default function userName() {
-    const readlineSync = require('readline-sync');
     const userName = readlineSync.question('May I have your name? ');
     console.log('Hi ' + userName + '!');
   };
