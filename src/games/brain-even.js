8232f3654dd91202990b07d0156726ccac8b452b
aleksandr <79081103268@yandex.ru>
2024-01-26 02:26:57 +0600

    brain even  worked

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,20 +1,18 @@
-import readlineSync from 'readline-sync';
 import mathGame from '../index.js';
 
 export default function randomEvenGame() {
-  const randomNumber = Math.floor(Math.random() * 100) + 1;
-  mathGame('Answer "yes" if the number is even, otherwise answer "no".',
-    function generateQuestion() {
-      return `${randomNumber}`;
-    },
-    function calculateCorrectAnswer(question) {
-      const isEven = randomNumber % 2 === 0;
-      return isEven ? 'yes' : 'no';
-    }
-  );
+  const generateQuestion = () => {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    return randomNumber;
+  };
+
+  const calculateCorrectAnswer = (question) => {
+    const isEven = question % 2 === 0;
+    return isEven ? 'yes' : 'no';
+  };
+
+  mathGame('Answer "yes" if the number is even, otherwise answer "no".', generateQuestion, calculateCorrectAnswer);
 }
-
-
 
 
 // //import readlineSync from 'readline-sync';
