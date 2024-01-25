d7d684d4fb3486d9b81f1e3cfd4b8a8549ca30cd
aleksandr <79081103268@yandex.ru>
2024-01-26 04:06:18 +0600

    brain-even finally worked

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export default function mathGame(gameDescription, generateQuestion, calculateCorrectAnswer) {
+export default function mathGame(description, generateQuestion, calculateCorrectAnswer) {
   console.log('Welcome to the Brain Games!');
   const yourName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${yourName}!`);
-  console.log(gameDescription);
+  console.log(`Hello, ${yourName}!\n${description}`);
 
   let correctAnswers = 0;
   let consecutiveCorrectAnswers = 0;
@@ -12,11 +11,11 @@ export default function mathGame(gameDescription, generateQuestion, calculateCor
 
   while (consecutiveCorrectAnswers < 3 && !gameOver) {
     const question = generateQuestion();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
     const correctAnswer = calculateCorrectAnswer(question);
 
-    const userAnswer = parseInt(readlineSync.question(`Question: ${question}\nYour answer: `), 10);
-
-    if (userAnswer === correctAnswer) {
+    if (userAnswer.toLowerCase() === correctAnswer) {
       console.log('Correct!');
       correctAnswers += 1;
       consecutiveCorrectAnswers += 1;
