f958a2fe9cfcf325922c556b44178d24bb652cd3
aleksandr <79081103268@yandex.ru>
2024-01-28 02:40:15 +0600

    progression didn't work

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,42 +1,63 @@
-import readlineSync from 'readline-sync';
+import mathGame from '../index.js';
 
-export default function arithmeticProgressionGame() {
-  const yourName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${yourName}!`);
-  console.log('What number is missing in the progression?');
-
-  let correctAnswers = 0;
-  let consecutiveCorrectAnswers = 0;
-  let gameOver = false;
-
-  while (consecutiveCorrectAnswers < 3 && !gameOver) {
+export default function randomEvenGame() {
+  const generateQuestion = () => {
     const progressionLength = Math.floor(Math.random() * 6) + 5;
     const hiddenElementPosition = Math.floor(Math.random() * progressionLength);
     const progressionStep = Math.floor(Math.random() * 10) + 1;
 
     let progression = '';
+  };
 
+  const calculateCorrectAnswer = (question) => {
     const correctAnswer = hiddenElementPosition * progressionStep;
     for (let i = 0; i < progressionLength; i += 1) {
-      const element = hiddenElementPosition === i ? '..' : i * progressionStep;
-      progression += `${element} `;
-    }
+    const element = hiddenElementPosition === i ? '..' : i * progressionStep;
+    progression += `${element} `;
+  };
 
-    const userAnswer = parseInt(readlineSync.question(`Question: ${progression}\nYour answer: `), 10);
-
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-      correctAnswers += 1;
-      consecutiveCorrectAnswers += 1;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      consecutiveCorrectAnswers = 0;
-      console.log(`Let's try again, ${yourName}!`);
-      gameOver = true;
-    }
-  }
-
-  if (correctAnswers === 3) {
-    console.log(`Congratulations, ${yourName}!`);
-  }
+  mathGame('Answer "yes" if the number is even, otherwise answer "no".', generateQuestion, calculateCorrectAnswer);
 }
+
+// import readlineSync from 'readline-sync';
+
+// export default function arithmeticProgressionGame() {
+//   const yourName = readlineSync.question('May I have your name? ');
+//   console.log(`Hello, ${yourName}!`);
+//   console.log('What number is missing in the progression?');
+
+//   let correctAnswers = 0;
+//   let consecutiveCorrectAnswers = 0;
+//   let gameOver = false;
+
+//   while (consecutiveCorrectAnswers < 3 && !gameOver) {
+//     const progressionLength = Math.floor(Math.random() * 6) + 5;
+//     const hiddenElementPosition = Math.floor(Math.random() * progressionLength);
+//     const progressionStep = Math.floor(Math.random() * 10) + 1;
+
+//     let progression = '';
+
+//     const correctAnswer = hiddenElementPosition * progressionStep;
+//     for (let i = 0; i < progressionLength; i += 1) {
+//       const element = hiddenElementPosition === i ? '..' : i * progressionStep;
+//       progression += `${element} `;
+//     }
+
+//     const userAnswer = parseInt(readlineSync.question(`Question: ${progression}\nYour answer: `), 10);
+
+//     if (userAnswer === correctAnswer) {
+//       console.log('Correct!');
+//       correctAnswers += 1;
+//       consecutiveCorrectAnswers += 1;
+//     } else {
+//       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+//       consecutiveCorrectAnswers = 0;
+//       console.log(`Let's try again, ${yourName}!`);
+//       gameOver = true;
+//     }
+//   }
+
+//   if (correctAnswers === 3) {
+//     console.log(`Congratulations, ${yourName}!`);
+//   }
+// }
