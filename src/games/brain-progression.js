618e37a446fd7856fd2b0db088d5a950e6d83daf
aleksandr <79081103268@yandex.ru>
2024-01-29 22:19:18 +0600

    11 edition finally worked

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,31 +1,29 @@
 import mathGame from '../index.js';
 
 export default function runArithmeticProgressionGame() {
-    const generateProgression = () => {
-      const startNumber = Math.floor(Math.random() * 50) + 1;
-      const step = Math.floor(Math.random() * 20) + 1;
-      const length = 10;
-      const progression = [startNumber];
-      for (let i = 1; i < length; i++) {
-        progression.push(progression[i - 1] + step);
-      }
-      return progression;
-    };
+  const generateProgression = () => {
+    const startNumber = Math.floor(Math.random() * 50) + 1;
+    const step = Math.floor(Math.random() * 20) + 1;
+    const length = 10;
+    const progression = [startNumber];
+    for (let i = 1; i < length; i += 1) {
+      progression.push(progression[i - 1] + step);
+    }
+    return progression;
+  };
 
-    let hiddenElement;
+  let hiddenElement;
 
-    const generateQuestion = () => {
+  const generateQuestion = () => {
     const progression = generateProgression();
     const index = Math.floor(Math.random() * (progression.length - 2)) + 1;
     hiddenElement = progression[index];
     progression[index] = '..';
-      return progression.join(' ');
-    };
-  
-    const calculateCorrectAnswer = (question) => {
-      return hiddenElement.toString();
-    };
-  
-    const description = 'What number is missing in the progression?';
-    mathGame(description, generateQuestion, calculateCorrectAnswer);
-  }
+    return progression.join(' ');
+  };
+
+  const calculateCorrectAnswer = () => hiddenElement.toString();
+
+  const description = 'What number is missing in the progression?';
+  mathGame(description, generateQuestion, calculateCorrectAnswer);
+}
